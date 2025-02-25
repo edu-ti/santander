971bb8fe8944473f824418353d94646bcce088c3
edu-ti <educs85.ti@gmail.com>
2025-02-25 10:43:18 -0300

    Adicionando pacotes e clases

---
+++ src/main/java/com/decolatech/santander/domain/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.decolatech.santander.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.decolatech.santander.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String number);
+
+    boolean existsByCardNumber(String number);
+}
